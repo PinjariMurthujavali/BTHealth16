ef329dfbf1fce8caf9c6e782a64becb452ec5713
P.MURTHUJAVALI <108625395+PinjariMurthujavali@users.noreply.github.com>
2026-03-25 13:05:49 +0530

    Add files via upload

---
+++ Health/health/report/lab_comparision_report/lab_comparision_report.js
@@ -0,0 +1,41 @@
+// Copyright (c) 2024, mj and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Lab Comparision Report"] = {
+	
+	"filters": [
+
+		{
+			"fieldname": "patient",
+			"label": __("Patient"),
+			"fieldtype": "Link",
+			"options":"Patient",
+			"reqd": 1
+		},
+		
+		{
+			"fieldname": "lab_test",
+			"label": __("Lab Test"),
+			"fieldtype": "Link",
+			"options":"Lab Test Template",
+			"reqd": 1
+		},
+	
+		{
+			"fieldname": "start_date",
+			"label": __("Start Date"),
+			"fieldtype": "Date",
+			"reqd": 1
+			
+		},
+
+		{
+			"fieldname": "end_date",
+			"label": __("End Date"),
+			"fieldtype": "Date",
+			"reqd": 1
+			
+		},
+		
+	]
+};
